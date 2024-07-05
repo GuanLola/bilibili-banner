12446fee65195fc1c31ae3e8c5a7fd4f6e29524d
Guan Yueming <3079756620@qq.com>
2024-07-05 10:23:21 +0800

    feat: 添加毕业生魔幻未来主题系列

---
+++ static_08/layers.js
@@ -0,0 +1,467 @@
+const layers = [
+  {
+    resources: [
+      {
+        src:
+          "./static_08/75ec2d45ce8c942a1f7379d4641171da4d90ab0d.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.54,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 0,
+    name: "19-背景水",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/286eb259a60a0eabfcde96d7ea92d239fe68b3fe.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.53,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 1,
+    name: "18-再远景",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/782d55aeca6cc75f51d2d630005f514a61a0ddfa.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.55,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 3,
+    name: "16-远景房子1",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/d6c941cf2d5fc6c717173f7e3f166dbc444aa15b.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [30, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 4,
+    name: "15-两侧房子",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/27e411d92729604aa594858beb5130ed60aad76d.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.55,
+      offset: 0.2,
+    },
+    rotate: {},
+    translate: {
+      offset: [30, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 2,
+    name: "17-远景鲸鱼机",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/09d0855b6b6d6965e8f02404777986237848c6c9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [300, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 5,
+    name: "14-中景鲸鱼机",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/40878bbef514e2d4bf5d660fe1145c869567bec2.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [20, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 6,
+    name: "13-窗外垃圾",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/42485baddbca05d2c4c7710a0b76b74d303e06d7.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.54,
+    },
+    rotate: {},
+    translate: {
+      offset: [80, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 7,
+    name: "12-机场",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/8ea0e95a8e5fc85ae227810925dba1ace1e9fcba.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [120, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 8,
+    name: "11-空姐",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/c13ca9c6405c71bf864ed2bc421680cb437f45ef.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [80, 40],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 9,
+    name: "10-泡泡04",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/a43c6833d262301373234ffbd6934559d2ce7fb2.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [100, 50],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 10,
+    name: "09-泡泡03",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/ce8c5e45230a6d3805baf60f5916f1cd441aac8e.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.54,
+    },
+    rotate: {},
+    translate: {
+      offset: [130, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 11,
+    name: "08-22",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/cbf19f3682dfb02e62557d07fefaf241a80296a1.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [200, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 12,
+    name: "07-近路人",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/4a4c1f6b2977478c73e41f39a2910c3b3c33167e.webm",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.5,
+    },
+    rotate: {},
+    translate: {
+      initial: [1000, 0],
+      offset: [20, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 21,
+    name: "右气泡",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/7998ca9f0bc267375fb7b45f75626d96806f94d7.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [300, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 13,
+    name: "06-两侧前景植物",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/198efffbc58493300854c04ab0ea8d979a6f9223.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [280, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 15,
+    name: "04-顶部摸鱼牌子",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/142a486b8dd500a626a60b68ad993af8dabc8b55.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [300, 130],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 16,
+    name: "03-泡泡02",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/bb6266e1525a51f7920fc8881e47cadeee271b0c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      initial: [200, 0],
+      offset: [350, 20],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 17,
+    name: "02-泡泡01",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/cd68251cde11936871237ca94360acb451bf7ed2.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      initial: [-200, 0],
+      offset: [500, 0],
+    },
+    blur: {},
+    opacity: {
+      initial: 0.5,
+      wrap: "clamp",
+    },
+    id: 18,
+    name: "01-光",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/426073f920477b718b8aee5ec141aca3889500f7.webm",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.54,
+    },
+    rotate: {},
+    translate: {
+      initial: [400, 0],
+      offset: [50, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 19,
+    name: "中气泡",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_08/0de9fb9822d2d00500abc8bdb143907eb1802ddb.webm",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      initial: [-700, 0],
+      offset: [30, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 20,
+    name: "左气泡",
+  },
+];
