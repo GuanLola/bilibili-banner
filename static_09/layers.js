4a6c9502375a205743105c3cce5f1466d6c3518e
Guan Yueming <3079756620@qq.com>
2024-10-10 09:39:34 +0800

    2024年10月份骑着扫帚飞主题

---
+++ static_09/layers.js
@@ -0,0 +1,498 @@
+const layers = [
+  {
+    resources: [
+      {
+        src:
+          "./static_09/18e14b236c8c0478b9f126b53dc149718c8277d6.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 0,
+    name: "=天空",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/2c048730ce3449400cd657650f5f2e68e2fa5ef5.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 1,
+    name: "-云层",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/582b9609daf669327c460af63776f5f0c14b5c26.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [20, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 2,
+    name: "-城市",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/f5fcc108b811577571e3f54d6d46df58a9f553ed.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.53,
+    },
+    rotate: {},
+    translate: {
+      offset: [40, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 3,
+    name: "-地表",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/4367bada261c826302101afa18fe41e86f545099.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [40, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 8,
+    name: "-远鸟群",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/0ebcccd16a9fe9bb551e213de4eaf3cd5a24c8ff.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [45, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 4,
+    name: "+左侧桥后",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/21a613a7457891ed7daa559b03191df7bf551362.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      initial: [200, 0],
+      offset: [500, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 7,
+    name: "+火车",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/83b58af34fc683e529962449078a3ded7f1d2e80.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [50, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 6,
+    name: "+火车铁路桥",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/6683112849dadee65749c4f7f72b83c9a9f8f537.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [60, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 5,
+    name: "+左侧桥前",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/93ab310ca3e2a24489aa1873865501adb3f81393.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      initial: [20, 0],
+      offset: [45, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 9,
+    name: "-右远高架桥",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/064a75156c568aa6a9982e2c983d2a30cb86ea94.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [70, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 10,
+    name: "-右近桥",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/7bb79a88a5c61724c7fd2ab009f462b744ba5b29.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.53,
+    },
+    rotate: {},
+    translate: {
+      offset: [50, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 11,
+    name: "-中景滩涂",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/1b6718fe3996574889da7ef1b9c29a566fe963f4.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.54,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 2],
+      offset: [75, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 12,
+    name: "-前景滩涂",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/c31995a7a80e459e165ae4ef60e323e27f497420.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [100, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 13,
+    name: "-前景右大柱子",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/4a66f79212d4c6182a8e10a1eb0424f3d7c79120.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [80, 5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 14,
+    name: "-中远鸟",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/30ccbdfe561bcd18a7ee457bd151b55da7a3dd3c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, -20],
+      offset: [400, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 15,
+    name: "-近鸟",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/20ccfde3e5606d223ab230a78234e4f9ac140371.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {
+      offset: 20,
+      offsetCurve: [0.115, 1.4889999999999999, 0.9455, -0.49650000000000016],
+    },
+    translate: {
+      initial: [500, 20],
+      offset: [400, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 21,
+    name: "=33",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/d562864113a6e6cce54b41745132a974b4576c3c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+      offset: -0.5,
+      offsetCurve: [1, 0.36149999999999993, 1, 0.42200000000000004],
+    },
+    rotate: {
+      offsetCurve: [0.489, 1.3955, 0.6705, -0.4195000000000002],
+    },
+    translate: {
+      initial: [200, 20],
+      offset: [300, -5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 20,
+    name: "=22",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/ab4fbaed481d880a618e52f97e7e1f6e5c8b7f7c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [600, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 16,
+    name: "-树叶",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/b399696aa4f7ebb732eae96368b35644359a1922.webm",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 20],
+      offset: [200, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 17,
+    name: "-左边植物",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/57f20e919517fc784f1e03900140274cfea2cb85.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [300, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 18,
+    name: "-左近桥",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_09/670d70b0351582e5b53855ddbf50600c03425062.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.52,
+    },
+    rotate: {},
+    translate: {
+      offset: [500, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 19,
+    name: "-近翅膀",
+  },
+];
