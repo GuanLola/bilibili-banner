6085ef61ac3be9b6f24f0ea324fa04efe5ad3e51
Guan Yueming <3079756620@qq.com>
2024-12-26 14:52:23 +0800

    feat: 新增冬日滑雪⛷主题

---
+++ static_10/layers.js
@@ -0,0 +1,506 @@
+const layers = [
+  {
+    resources: [
+      {
+        src:
+          "./static_10/477fb7a2f5e2cc1b2aa00c679c8ab168b47ff1b9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [3, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 7,
+    name: "23背景",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/dd4aa5af4898a15dde074fe6b833bdfbc045dd47.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {
+      offset: 5,
+    },
+    translate: {
+      initial: [-50, 0],
+      offset: [10, -4],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 8,
+    name: "22龙头",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/ae64c474ba2748b4fea1a1433b9b373598d3e686.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 9,
+    name: "21中间远山",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/d41b0292d9d1b0fce37a35f6efacac579093cdb3.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {
+      offset: 5,
+    },
+    translate: {
+      initial: [-70, -20],
+      offset: [15, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 10,
+    name: "20龙段2",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/a12cbab877db2b6d3b96ec012735cfe3998de399.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      offset: [-10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 12,
+    name: "18左侧山",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/6f27fe80f495e83546921c2c58b2107c2ee89706.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.95,
+      offset: 0.05,
+    },
+    rotate: {},
+    translate: {
+      initial: [10, 0],
+      offset: [25, 5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 11,
+    name: "19龙段1",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/ac580ac636595e27b5d5c8b1f9d802751823f02f.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      offset: [-40, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 13,
+    name: "17左侧山坡",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/987a407da2e02a12be549ad7f011dc11f73216e9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 10],
+      offset: [-15, 3],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 14,
+    name: "16地面垃圾",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/843ee84b16bab6274b98b317b3adaf1351b4be76.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      initial: [10, 0],
+      offset: [30, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 15,
+    name: "15缆车电线",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/a5607001ac06b4780106e2a10aa835657e553f07.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      initial: [10, 0],
+      offset: [-20, 35],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 16,
+    name: "14缆车箱体",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/b14cad3c76e957705810c26f4c6acc236f280721.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      initial: [20, 0],
+      offset: [15, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 17,
+    name: "13右侧中远山",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/f569be6834a5968c038bcc0fb0403f8f77e24c18.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      initial: [10, 0],
+      offset: [20, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 18,
+    name: "12右侧电线",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/eb98a42561de7d45f8856407c4531d2724ac6fc6.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.7,
+      offset: -0.3,
+    },
+    rotate: {
+      offset: -5,
+    },
+    translate: {
+      initial: [30, 30],
+      offset: [100, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 19,
+    name: "11右侧鸟",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/480a5c02dbcd3afd5f790cf621efbd1b2d39efcb.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      offset: [50, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 20,
+    name: "10右侧山体",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/cc1405d4d805baf9c459f67c03f095f70ec864c6.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 20],
+      offset: [15, -5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 21,
+    name: "09垃圾",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/1696ffeacb26bcce7ad50037952620ee43614a4a.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+      offset: -0.2,
+    },
+    rotate: {},
+    translate: {
+      offset: [20, 8],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 22,
+    name: "08垃圾鸟群",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/b0747b7f64e0e07e379a4bd416a0891b3e52c7d9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.86,
+    },
+    rotate: {},
+    translate: {
+      initial: [40, 0],
+      offset: [60, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 6,
+    name: "07_33和山坡",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/ec9f99bf71be9408a359d7cd48dfb47e48ebce2c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+      offset: -0.3,
+    },
+    rotate: {
+      offset: 20,
+    },
+    translate: {
+      offset: [25, -17],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 5,
+    name: "06左侧鸟",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/c4cef2b063a853521c7f413d8e7782ff653e4126.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      initial: [20, 0],
+      offset: [80, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 4,
+    name: "05_22",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/567e0221b3bd89e34d85c604549d915c95ca17d1.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      initial: [30, 0],
+      offset: [-100, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 3,
+    name: "04最左遮挡",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/23c05e4d9e0ffbc16671e14c406fbe785b06ff9f.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.85,
+    },
+    rotate: {},
+    translate: {
+      initial: [18, 20],
+      offset: [90, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 2,
+    name: "03中间石头",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_10/fbfeb5de7e41fd1e888fca7ac76cbf2f8a42f3d3.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [100, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 1,
+    name: "02最右石头",
+  },
+];
