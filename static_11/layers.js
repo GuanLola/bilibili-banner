f4f391c8cb7b099ebbbd7ecc9ba2bf217a9b4dac
Guan Yueming <3079756620@qq.com>
2025-04-03 17:44:59 +0800

    feat: 新增2025年4月份-踏春系列

---
+++ static_11/layers.js
@@ -0,0 +1,389 @@
+const layers = [
+  {
+    resources: [
+      {
+        src:
+          "./static_11/5598c26a3d090ba7cdf1d99de1e7d76561d56e78.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.87,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 0,
+    name: "17_天空",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/556c868c5b003f3f167cf1e63e6e3aa27b74bdc7.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.87,
+    },
+    rotate: {},
+    translate: {
+      offset: [3, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 2,
+    name: "16_云",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/b1b6ebe5d3def015132105fe7a8253eced9b563c.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      initial: [0, 5],
+      offset: [20, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 4,
+    name: "14_33",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/59a63bc51e049fd01d7e567b9759727888d0de43.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.87,
+    },
+    rotate: {},
+    translate: {
+      offset: [5, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 5,
+    name: "13左侧垫底",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/2e5a5407987125451f4401e1f80da8aa6c9ec74e.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.87,
+    },
+    rotate: {},
+    translate: {
+      initial: [-270, -20],
+      offset: [20, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 6,
+    name: "12车子",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/96151dfbace0750a4a590e9fac92211ce7c28b82.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.95,
+    },
+    rotate: {},
+    translate: {
+      initial: [-50, 0],
+      offset: [15, -5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 7,
+    name: "11左侧建筑",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/f272c9c3125afec158fe4c4e0540a271cd75dfb4.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {
+      offset: 10,
+    },
+    translate: {
+      initial: [-250, -20],
+      offset: [30, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 3,
+    name: "15_22",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/598389b103a4c89592706b9a426c1dd5c9fbdb96.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [30, 5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 8,
+    name: "10中间花田",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/3dd93e33d7e9bb6b74b7e2c577cb4a61cd7876bf.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.87,
+    },
+    rotate: {},
+    translate: {
+      offset: [50, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 9,
+    name: "09兔子",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/2ec5703e20135d578a4d31a9d40dd9deb23c326e.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.95,
+    },
+    rotate: {},
+    translate: {
+      offset: [20, 5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 10,
+    name: "08右侧草3",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/48d675bee7f2b20af495ddcfa2051d1e42eeb526.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [40, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 11,
+    name: "07右侧草2",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/e35035fb17abb07d80c1749988bc938c9a5919dc.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 1.1,
+    },
+    rotate: {},
+    translate: {
+      offset: [70, 15],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 12,
+    name: "06左下花草",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/5cbcbeb21c079264f78e63c411f09334126a11d3.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 1.1,
+    },
+    rotate: {},
+    translate: {
+      offset: [80, 15],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 13,
+    name: "05左上树叶",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/c9ef0b13e5ec27cc6e9fa1516e79a8ab8f5c5b38.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 1.1,
+    },
+    rotate: {},
+    translate: {
+      offset: [90, 15],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 14,
+    name: "04右上树叶",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/5a97b3d3f84e4ce0e77988a8a0d5744a2a2fd8b9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.87,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 15,
+    name: "03垃圾",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/679c3bb077504de3379100c688770d94d97778cf.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [70, 8],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 16,
+    name: "02右侧草1",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/add3317b33f371b4ce4391b5169dbd6455065ed4.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [60, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 17,
+    name: "01前景草1",
+  },
+  {
+    resources: [
+      {
+        src:
+          "./static_11/c4807a30a39d8d5df2692c8fb40797d51652dbc6.webm",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [60, 0],
+    },
+    blur: {
+      initial: 1,
+    },
+    opacity: {
+      initial: 0.5,
+      wrap: "clamp",
+    },
+    id: 18,
+    name: "图层18",
+  },
+];
