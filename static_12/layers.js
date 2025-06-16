dc67c8de2e3821d3619da4e0d3112644a291ac0a
3079756620@qq.com <3079756620@qq.com>
2025-06-16 18:46:25 +0800

    feat: 添加毕业季主题

---
+++ static_12/layers.js
@@ -0,0 +1,570 @@
+const layers = [
+  {
+    resources: [
+      {
+        src: "./static_12/081110f6c4c54ab35e31cf0eb7bd0b129ee4135c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 0,
+    name: "27天空",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/72d21b0354fd65c2cd7887aeab0eab826b690015.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [1, 5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 1,
+    name: "26云",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/35ba492c9e7cab77772f88849f3653b1834c0584.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [2, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 2,
+    name: "25两侧山",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/c5c495a0da944d2c58573427e456522e9969628c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [4, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 3,
+    name: "24中间山",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/0555dd834b4c9190122b62c6d50166a6c85156cc.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [3, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 4,
+    name: "23大山",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/755e692231b4274e59592962ef3f00176a0dd8d9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      initial: [200, 0],
+      offset: [5, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 5,
+    name: "22破车",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/8960819e5fcb64a6710c047e2ed0bfce4ef4ef82.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [5, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 6,
+    name: "21电线杆组",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/edd44095c190003338a34434b61eb80309eac6f3.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [6, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 7,
+    name: "20超市",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/deaf8e5b8cc54b0db6cf3ee947f41dafbaa0d556.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [5, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 8,
+    name: "19水",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/5900e700709d4a466dd6e511522d9c2d4c1b44b9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [5, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 9,
+    name: "18零散",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/7d1e3a2703b9b6912aafaa2aa780ec59697be960.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [5, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 10,
+    name: "17左边缘草",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/7beeb9d813e9d35a90effa7d260aa2f655effe1d.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 11,
+    name: "16水分割",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/5cc5ec4d267654a75804ed3df991b8e135420eba.webm",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.8,
+    },
+    rotate: {},
+    translate: {
+      initial: [-900, 0],
+      offset: [20, -5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 12,
+    name: "15柚子",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/2d8870bf565d21e7247d74b5a85d733b4948028b.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, -2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 13,
+    name: "14投影22",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/9b48ea558d3288dd2ee98f088fcf1ee94d475d01.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [20, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 14,
+    name: "13投影33",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/78ea0ded394443ea6e70231b7b92f574ce38e098.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [15, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 15,
+    name: "12板子",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/d20b70bcbd05bd1b2f3d5f6f0c820b79eabcdbd6.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [14, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 16,
+    name: "11-22",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/1d315705f73e71657c5be46bdfbccc2244990402.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [20, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 17,
+    name: "10-33",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/50762a0fc7d3672b176e29383743265dc305b09c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [-15, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 18,
+    name: "09右边板子",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/7b3a50409b380a6e79afc291144469bd73b0d27a.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [20, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 19,
+    name: "08游泳圈",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/4f3b8105cb124ac0d9eab473426e5aed5fa2cff2.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [40, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 20,
+    name: "07右草",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/61f6761032904ac8b9f57f24c34b734f1767c4b1.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 21,
+    name: "06远处鸭子",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/49c634d8888d66a6d4c9ce4ac39d37ee21a5a45f.webm",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.8,
+    },
+    rotate: {},
+    translate: {
+      initial: [500, 40],
+      offset: [60, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 22,
+    name: "05右前鸭子",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/eabcd7037e15b091500488a4223899aa05af01a4.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      initial: [-40, 0],
+      offset: [20, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 23,
+    name: "04左草",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/ac2460c804294c9f874aa21a53084ac7dda9aba9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [40, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 24,
+    name: "03草中",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/ea2d126e7218ea30fefd22d2d2f5ba394bc89158.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      offset: [60, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 25,
+    name: "02右前电线杆",
+  },
+  {
+    resources: [
+      {
+        src: "./static_12/4535c0cc4efd7bf940c7ba245c5bfd71b1bf7b32.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.9,
+    },
+    rotate: {},
+    translate: {
+      initial: [-125, 0],
+      offset: [80, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 26,
+    name: "01左前电线杆",
+  },
+];
+
+export default layers;
