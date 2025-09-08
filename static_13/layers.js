d94650995722c2d62acb60bdd2db967cd0375b4c
3079756620@qq.com <3079756620@qq.com>
2025-09-08 16:00:58 +0800

    feat: 添加月亮海滩主题

---
+++ static_13/layers.js
@@ -0,0 +1,491 @@
+const layers = [
+  {
+    resources: [
+      {
+        src: "./static_13/90240f707cb4a015bbf8bbd13e018b3f664087ce.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+      offset: 0.02,
+    },
+    rotate: {},
+    translate: {
+      offset: [40, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 0,
+    name: "21天空",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/cd4194a7be89655450147d2384a162b966cf2ec2.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [5, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 1,
+    name: "20远景色",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/ab2379f7c80b225020ee42289db11b84b57c2766.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+      offset: -0.02,
+    },
+    rotate: {},
+    translate: {
+      offset: [5, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 2,
+    name: "19月亮",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/938b58321d184fde31c783f5b321621ffb0c190e.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [8, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 3,
+    name: "18沙滩1",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/af840eae2cc555b0757d406e252f7a7dd6542eed.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 4,
+    name: "17投影桥",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/1271b110ca83ef84bf8d2c664537c8644a273216.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [15, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 5,
+    name: "16投影碎石",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/c8b49dc1aa86b2573ce2736de6692acfe0182481.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 6,
+    name: "15星星投影+反光",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/0881f755a4857a3b9bc12c3bbe41c00382ac6fc6.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [30, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 7,
+    name: "14投影33+狗",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/065fff3eb5ce38fd2d3a658ff825d5aa3a744e73.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [9, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 8,
+    name: "13沙滩2",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/7e61b8ea5efd98ade40b7ee386dd1bbc2b5898f5.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+      offset: 0.01,
+    },
+    rotate: {},
+    translate: {
+      offset: [6, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 9,
+    name: "12海水",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/27724404973bbe4573bfabab6fed6767d6b06815.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+      offset: 0.02,
+    },
+    rotate: {},
+    translate: {
+      offset: [15, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 10,
+    name: "11海浪",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/29d11ebfdd1b9d0840cc528c95ede27fe3d0a8e2.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 11,
+    name: "10左侧垃圾",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/3af7aa17868b8e5bc26950ac4a399bec62b83e50.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 12,
+    name: "09月光",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/27ec840f903725d7ad7ad8356d37ead40ece4b31.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+      offset: 0.01,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 13,
+    name: "08桥",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/230bdd9372f4d1362d0d9cd75d3b233b2db29d43.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [15, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 14,
+    name: "07沙滩上的石头",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/56f088b30dadc2c99fed255fcf4cc34c4f37f313.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [30, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 15,
+    name: "0633与狗",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/c5a4a63c098b81d89c73d359de35fcea9bb1c7a3.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 21,
+    name: "00沙滩碎星星",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/d195a834c55a24c1f599e7c15e3b59e5795c5c0f.webm",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.5,
+    },
+    rotate: {},
+    translate: {
+      initial: [-205, 65],
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 23,
+    name: "动态小星星",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/59b1c59d919469fc0a4632acc5a6eecd9260d099.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [35, 10],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 17,
+    name: "0422",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/7b7dd8a92bf8036be6502898e9804c65ee0f6284.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      initial: [-5, -5],
+      offset: [38, 13],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 18,
+    name: "0322手里星星",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/aabd831214cdae044414980e3c06787c0f3073ff.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [100, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 19,
+    name: "02最前景石头",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/b5b5336124932336e39a99d64014fc0154d53912.webm",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      initial: [-1140, 130],
+      offset: [130, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 22,
+    name: "05前景星星",
+  },
+  {
+    resources: [
+      {
+        src: "./static_13/928547de5ced33ce2b28b7924e1a470a110eed26.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.47,
+    },
+    rotate: {},
+    translate: {
+      offset: [-5, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 20,
+    name: "01流星",
+  },
+];
+
+export default layers;
