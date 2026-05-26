4a5571a4be174e886698f62d1a472c64d566e9c3
cnb <86yJykfP4lbNk8uO9E5y2E+guanyueming@noreply.cnb.cool>
2026-05-26 09:32:44 +0800

    feat:5月份春夏主题

---
+++ static_15/layers.js
@@ -0,0 +1,695 @@
+const layers = [
+  {
+    resources: [
+      {
+        src: "/static_15/7ba90052bb0a7d52ff2e2651c58325176823dab7.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 0,
+    name: "033bg",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/345b6e0fe3bcce70b4dda85984e6bd2e989ed383.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 1,
+    name: "032云层",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/943d6df51d9698d2bbccfd1420a91e42c0bdb8ce.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [12, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 3,
+    name: "030右边电线",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/7df8b2e7038b224ce684694d05e55ac537534f97.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 4,
+    name: "029山",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/d3a3efde2411a0a79421ca9e7a7fa5ffa6a377e8.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [14, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 6,
+    name: "027右边树丛",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/d6f0810cff9d297fb5de5dcd474b1303cc8efc56.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [16, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 7,
+    name: "026水塘",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/e3442b0c6d5b098a0577ae9a52fe3f862e253457.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [12, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 8,
+    name: "025尾巴",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/b45d1213022a666a43a8a826a05ba449ca14f5ae.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [13, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 9,
+    name: "024二节",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/fe1d49a21d9715906fd580a09f5dab5a4d8762b9.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 10,
+    name: "023一节",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/aaed1e390b9cd7a7cf04da45cae135856b66df8f.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 11,
+    name: "022石头",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/2b343db84d1754f9ad69a761d1a0043be367b134.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [8, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 12,
+    name: "021尾巴",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/3a951ea0bd0b2d1c7972f06fd087efdeef037094.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 13,
+    name: "020轨道",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/16a4c20e2961a5b44ef84d93ddc52c99f7010098.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: 0.02,
+    },
+    rotate: {},
+    translate: {
+      offset: [0, -5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 18,
+    name: "=015左侧风车",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/86b62f4fb731a12e50450fe3e42db9b156c868fb.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [15, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 2,
+    name: "031鸟群",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/ba7e7f012036d1249eca78ae542733e577e4f967.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: -0.003,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 2],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 14,
+    name: "019第四段",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/9e3cce6670857a16e82e9d8ec4ce5fdf42eee0c7.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: -0.003,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 5],
+      offset: [10, -3],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 15,
+    name: "018第三段",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/e37c55a1af51dd92494a45beecce6ef2a9b53376.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: 0.006,
+    },
+    rotate: {},
+    translate: {
+      offset: [0, -5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 16,
+    name: "017第二段",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/047dbf50c727274d4ce3c661da05817849bf9ed5.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: 0.01,
+    },
+    rotate: {},
+    translate: {
+      offset: [10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 17,
+    name: "016最右模糊车厢",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/e1854d407c84a2d3945c9a3be469088ad609f419.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: 0.025,
+    },
+    rotate: {},
+    translate: {
+      offset: [3, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 19,
+    name: "014左侧远景",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/297bf9dcb49425d3a82ace6344ee6e5c9ea5f024.png",
+        id: 0,
+      },
+    ],
+    scale: {},
+    rotate: {},
+    translate: {
+      offset: [5, 5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 20,
+    name: "013左侧地皮",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/7d88d35026b2ea610161b96140e5be87c9ccbcad.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [-20, -20],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 21,
+    name: "012左侧标志牌",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/ff94ad93d5408a0c69d5193857233c42b1b10b41.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 10],
+      offset: [5, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 22,
+    name: "011中景远树丛",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/ad9c11a699e68a8359dff42510f50068ae044245.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.4306,
+      offset: 0.03,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 15],
+      offset: [-10, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 23,
+    name: "010左侧中景树丛",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/039d4f612147dbc315d4d75f0662c2bd0747559c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [-15, 5],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 24,
+    name: "009左侧中景地皮",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/ebdf8eb6f2bbcf8f45e77e1b481ef955274451d5.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 5],
+      offset: [-15, 8],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 25,
+    name: "=008右侧中景地皮",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/ef3ff7abf1330d4cfb4897390a08ca718c37dc76.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: 0.03,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 25],
+      offset: [20, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 26,
+    name: "007右边中景树",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/457ee6eb3e494f6151342e13ff21f4241312f770.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      initial: [0, 20],
+      offset: [25, 8],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 27,
+    name: "006右边高台",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/b3a1001bc64e2a14f35fbb47d791993732f67367.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+    },
+    rotate: {},
+    translate: {
+      offset: [50, -6],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 28,
+    name: "005右边桩子",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/0099698cfeea68b59a323f7c1248df6a7485a045.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: 0.2,
+    },
+    rotate: {
+      offset: 10,
+    },
+    translate: {
+      offset: [-150, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 29,
+    name: "004_33",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/625a4870c6da681bcecddaeae52f7ce912a38ebd.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: -0.1,
+    },
+    rotate: {
+      offset: -10,
+      offsetCurve: [0.6815000000000001, 0.02049999999999985, 0.995, 0.818],
+    },
+    translate: {
+      initial: [-600, 0],
+      offset: [220, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 30,
+    name: "003_22",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/cf47e267578fd0dc1bce63e5f8e204f02537bf25.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.45,
+      offset: -0.05,
+    },
+    rotate: {},
+    translate: {},
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 31,
+    name: "002左边前景树",
+  },
+  {
+    resources: [
+      {
+        src: "/static_15/9e02ff62f84d5e53f416620caa18aa3f63a48a4c.png",
+        id: 0,
+      },
+    ],
+    scale: {
+      initial: 0.5,
+      offset: 0.06,
+    },
+    rotate: {},
+    translate: {
+      initial: [200, 0],
+      offset: [120, 0],
+    },
+    blur: {},
+    opacity: {
+      wrap: "clamp",
+    },
+    id: 32,
+    name: "001右边前景树",
+  },
+];
+
+export default layers;
